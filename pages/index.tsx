b3c0aff8474f8e5a096f124628c33f7f97d7ed1b
Quentin Hoog <quentin.hoog@gmail.com>
2021-10-04 02:11:48 +0200

    Improve parallax

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,22 +20,12 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <main>
         <Parallax ref={parallax} pages={3}>
           <ParallaxLayer offset={0.4} speed={0.6} style={{ backgroundColor: '#01202a' }} />
   
           {/* Presentation */}
-          <ParallaxLayer offset={0} speed={1} factor={1}
-            style={{
-              background: '#022A36',
-              color: 'white',
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-              paddingLeft: '4rem',
-              paddingRight: '4rem',
-            }}
-          />
+          <ParallaxLayer offset={0} speed={1} factor={0.8} style={{ background: '#022A36' }} />
 
           <ParallaxLayer offset={0} speed={2} factor={3} style={{
               backgroundImage: url('stars', true),
@@ -43,7 +33,7 @@ const Home: NextPage = () => {
             }}
           />
 
-          <ParallaxLayer offset={0.1} speed={-0.15} factor={6} style={{paddingTop: '8rem',}}>
+          <ParallaxLayer offset={0} speed={-0.4} factor={1} style={{paddingTop: '8rem',}}>
             <div className={styles.presentationTextContainer}>
               <h1>Quentin HOOG</h1>
               <p>Developer</p>
@@ -51,32 +41,54 @@ const Home: NextPage = () => {
             </div>
           </ParallaxLayer>
 
-          <ParallaxLayer offset={0.2} speed={0} style={{ pointerEvents: 'none' }}>
-            <img src={url('satellite4')} style={{ width: '15%', marginLeft: '70%' }} />
-          </ParallaxLayer>
+          <ParallaxLayer offset={0.2} speed={0.3} style={{ pointerEvents: 'none' }}>
+            <div className={styles.imgSateliteContainer}>
+              <Image src="/images/satelite.png" alt="Satelite" width={140} height={140} />
+            </div>
+          </ParallaxLayer> 
           
           {/* About */}
           <ParallaxLayer offset={1} speed={1} style={{
               background: '#0A5871',
-              color: 'white',
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'space-evenly',
-              paddingLeft: '4rem',
-              paddingRight: '4rem',
+              
           }}>
-            <article className={styles.articleAbout}>
-              <h2>About</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel magna 
-                vel nisl imperdiet dapibus. Vestibulum feugiat bibendum vulputate. 
-                Pellentesque tincidunt turpis ac justo laoreet tempor.
-              </p>
-            </article>
-            <article className={styles.articleSkills}>
-              <h2>Skills</h2>
-            </article>
+            <section className={styles.about}>
+              <article className={styles.articleAbout}>
+                <h2>About</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel magna 
+                  vel nisl imperdiet dapibus. Vestibulum feugiat bibendum vulputate. 
+                  Pellentesque tincidunt turpis ac justo laoreet tempor.
+                </p>
+              </article>
+              <article className={styles.articleSkills}>
+                <h2>Skills</h2>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel magna 
+                  vel nisl imperdiet dapibus. Vestibulum feugiat bibendum vulputate. 
+                  Pellentesque tincidunt turpis ac justo laoreet tempor.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel magna 
+                  vel nisl imperdiet dapibus. Vestibulum feugiat bibendum vulputate. 
+                  Pellentesque tincidunt turpis ac justo laoreet tempor.
+                </p>
+              </article>
+            </section>
+          </ParallaxLayer>
+
+          <ParallaxLayer offset={1.5} speed={3} style={{ pointerEvents: 'none'}}>
+            <Image src="/images/spaceship.svg" alt="Spaceship" width={120} height={140} />
           </ParallaxLayer>
           
+          <ParallaxLayer offset={1.6} speed={0.5} style={{ opacity: 0.1 }}>
+            <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '70%' }} />
+            <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '40%' }} />
+          </ParallaxLayer>
+
+          <ParallaxLayer offset={1.9} speed={0.8} style={{ 
+            backgroundColor: '#1d80a1a6', 
+            height: '80vh', 
+          }} />
+
           {/* Contact */}
           <ParallaxLayer offset={2} speed={1} style={{
             background: '#50B7D8',
@@ -88,6 +100,22 @@ const Home: NextPage = () => {
           }}>
             <h2 className={styles.contactH2}>Contact me</h2>
           </ParallaxLayer> 
+
+          <ParallaxLayer offset={2} speed={0.8} style={{ opacity: 0.1 }}>
+            <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '55%' }} />
+            <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '15%' }} />
+          </ParallaxLayer>
+
+          <ParallaxLayer offset={2.75} speed={0.5} style={{ opacity: 0.1 }}>
+            <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '70%' }} />
+            <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '40%' }} />
+          </ParallaxLayer>
+
+          <ParallaxLayer offset={2} speed={0.2} style={{ opacity: 0.2 }}>
+            <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '10%' }} />
+            <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '75%' }} />
+          </ParallaxLayer>
+
 
         </Parallax>
       </main>
