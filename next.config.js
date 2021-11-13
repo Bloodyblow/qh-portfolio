c38a1f7e46c83c831eaa6e5ca338c55e03c0314a
Quentin Hoog <quentin.hoog@gmail.com>
2021-11-13 22:54:50 +0100

    Accessibility

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  i18n: {
+    locales: ["en", "fr"],
+    defaultLocale: "en",
+  },
   images: {
     loader: 'custom',
   },
